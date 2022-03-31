2dfb2e6d3291a464ba6019df50c0b59a25a8595e
Pedro Mascimo <pedromascimo@hotmail.com>
2022-03-31 11:28:19 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+$('.procurarContent').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 980,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+});
+
+
+
+
 
 $('.carousel').slick({
     infinite: true,
