353b7e850f117d2e48870255a930ce0cbb4424e7
Pedro Mascimo <pedromascimo@hotmail.com>
2022-04-09 11:58:55 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ $('.procurarContent').slick({
 
 // REDUZIR
 
-$('.carousel').slick({
+$('.carousel, .carousel03').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -54,27 +54,27 @@ $('.carousel').slick({
 
 
 
-$('.carousel03').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1100,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                centerMode: true,
-                variableWidth: true,
-            }
-        },
-    ]
-});
+// $('.carousel03').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     responsive: [
+//         {
+//             breakpoint: 1100,
+//             settings: {
+//                 slidesToShow: 2,
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 1,
+//                 centerMode: true,
+//                 variableWidth: true,
+//             }
+//         },
+//     ]
+// });
 
 
 
